4bf3d781998613b0dce71205b07b3dc0f203ab42
JKJM1 <krasotochnaya94@gmail.com>
2024-04-15 12:59:09 +0700

    PR3 ОФК

---
+++ C#/Пр3 ОКФ/Figura.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Пр3_ОКФ
+{
+    internal class Figura
+    {
+        int x;
+        int y;
+        int h;
+        PictureBox pb;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public int H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+        public Figura(int nx,int ny,int nh, PictureBox npb) 
+        {
+            x= nx;
+            y= ny;
+            h = nh;
+            pb = npb;
+            Draw();
+        }
+        public void Draw()
+        {
+            Graphics gr = Graphics.FromImage(pb.Image);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            gr.FillRectangle(blueBrush, x, y, h, h);
+
+            Point[] triangle1Points = { new Point(x, y), new Point(x + h / 2, y + h / 2), new Point(x, y + h) };
+            Point[] triangle2Points = { new Point(x + h, y), new Point(x + h / 2, y + h / 2), new Point(x + h, y + h) };
+            Point[] triangle3Points = { new Point(x, y), new Point(x + h / 2, y + h / 2), new Point(x + h, y) };
+            Point[] triangle4Points = { new Point(x, y + h), new Point(x + h / 2, y + h / 2), new Point(x + h, y + h) };
+            Pen redPen = new Pen(Color.Red, 2);
+            gr.DrawPolygon(redPen, triangle1Points);
+            gr.DrawPolygon(redPen, triangle2Points);
+            gr.DrawPolygon(redPen, triangle3Points);
+            gr.DrawPolygon(redPen, triangle4Points);
+            SolidBrush greenBrush = new SolidBrush(Color.Green);
+            int circleDiameter = h / 6;
+            Point circleCenter = new Point(x + h / 3 + circleDiameter, y + h / 4); 
+            gr.FillEllipse(greenBrush, circleCenter.X - circleDiameter / 2, circleCenter.Y - circleDiameter / 2, circleDiameter, circleDiameter);
+            Point circleCenter2 = new Point(x + h / 3 + circleDiameter, y + 3 * h / 4);
+            gr.FillEllipse(greenBrush, circleCenter2.X - circleDiameter / 2, circleCenter2.Y - circleDiameter / 2, circleDiameter, circleDiameter);
+            pb.Refresh();
+        }
+        public void Edit(int nx, int ny, int nh)
+        {
+            x = nx;
+            y = ny;
+            h = nh;
+            Draw();
+        }
+
+        public void Delete()
+        {
+            Graphics gr = Graphics.FromImage(pb.Image);
+            gr.Clear(Color.Transparent);
+            pb.Refresh();
+        }
+    }
+}
---
+++ C#/Пр3 ОКФ/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Пр3_ОКФ
+{
+    public partial class Form1 : Form
+    {
+        private int currentX;
+        private int currentY;
+        private int currentH;
+        private bool addEnabled = true;
+        private bool editEnabled = true;
+        private bool deleteEnabled = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bp;
+            Graphics gr= Graphics.FromImage(bp);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush,0,0,pictureBox1.Width,pictureBox1.Height);
+            pictureBox1.Refresh();
+        }
+        public void DrawFigures()
+        {
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Refresh();
+            foreach (Figura figura in listBox1.Items)
+            {
+                figura.Draw();
+            }
+            pictureBox1.Refresh();
+        }  
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            try
+            {
+                if (addEnabled)
+                {
+                    if (f.ShowDialog() == DialogResult.OK)
+                    {
+                        int x = Convert.ToInt32(f.textBox1.Text);
+                        int y = Convert.ToInt32(f.textBox2.Text);
+                        int h = Convert.ToInt32(f.textBox3.Text);
+                        Figura fg = new Figura(x, y, h, pictureBox1);
+                        listBox1.Items.Add(fg);
+                        DrawFigures();
+                    }
+                    else
+                    {
+                        throw new Exception("Добавление объекта заблокировано.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (deleteEnabled)
+                {
+                    if (listBox1.SelectedItem != null)
+                    {
+                        Figura selectedFigura = listBox1.SelectedItem as Figura;
+                        selectedFigura.Delete();
+                        listBox1.Items.Remove(selectedFigura);
+                        DrawFigures();
+                    }
+                    else
+                    {
+                        throw new Exception("Не выбран объект для удаления.");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Удаление объекта заблокировано.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (editEnabled)
+                {
+                    if (listBox1.SelectedItem != null)
+                    {
+                        Form2 f = new Form2();
+
+                        // Получаем текущие значения цифр объекта перед открытием формы редактирования
+                        Figura selectedFigura = listBox1.SelectedItem as Figura;
+                        currentX = selectedFigura.X;
+                        currentY = selectedFigura.Y;
+                        currentH = selectedFigura.H;
+
+                        // Передаем текущие значения в форму редактирования
+                        f.textBox1.Text = currentX.ToString();
+                        f.textBox2.Text = currentY.ToString();
+                        f.textBox3.Text = currentH.ToString();
+
+                        if (f.ShowDialog() == DialogResult.OK)
+                        {
+                            int x = Convert.ToInt32(f.textBox1.Text);
+                            int y = Convert.ToInt32(f.textBox2.Text);
+                            int h = Convert.ToInt32(f.textBox3.Text);
+
+                            // Если значения не изменились, используем сохраненные значения
+                            if (x == currentX && y == currentY && h == currentH)
+                            {
+                                selectedFigura.Edit(currentX, currentY, currentH);
+                            }
+                            else
+                            {
+                                selectedFigura.Edit(x, y, h);
+                            }
+                            DrawFigures();
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Не выбран объект для редактирования.");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Редактирование объекта заблокировано.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+    }   
+}
