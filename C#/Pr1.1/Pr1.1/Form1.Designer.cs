4a72a9486ef424c4c5a812b6040830602aca9961
JKJM1 <krasotochnaya94@gmail.com>
2025-01-18 09:44:06 +0700

    pr1.1

---
+++ C#/Pr1.1/Pr1.1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Pr1._1
+{
+    partial class ОС
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dataGridViewSystemInfo = new System.Windows.Forms.DataGridView();
+            this.Значение = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Свойство = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSystemInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(623, 386);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(165, 52);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dataGridViewSystemInfo
+            // 
+            this.dataGridViewSystemInfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewSystemInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSystemInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Значение,
+            this.Свойство});
+            this.dataGridViewSystemInfo.Location = new System.Drawing.Point(48, 12);
+            this.dataGridViewSystemInfo.Name = "dataGridViewSystemInfo";
+            this.dataGridViewSystemInfo.ReadOnly = true;
+            this.dataGridViewSystemInfo.RowHeadersVisible = false;
+            this.dataGridViewSystemInfo.RowHeadersWidth = 51;
+            this.dataGridViewSystemInfo.RowTemplate.Height = 24;
+            this.dataGridViewSystemInfo.Size = new System.Drawing.Size(651, 355);
+            this.dataGridViewSystemInfo.TabIndex = 1;
+            // 
+            // Значение
+            // 
+            this.Значение.HeaderText = "Значение";
+            this.Значение.MinimumWidth = 6;
+            this.Значение.Name = "Значение";
+            this.Значение.ReadOnly = true;
+            // 
+            // Свойство
+            // 
+            this.Свойство.HeaderText = "Свойство";
+            this.Свойство.MinimumWidth = 6;
+            this.Свойство.Name = "Свойство";
+            this.Свойство.ReadOnly = true;
+            // 
+            // ОС
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewSystemInfo);
+            this.Controls.Add(this.btnExit);
+            this.Name = "ОС";
+            this.Text = "ОС";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSystemInfo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dataGridViewSystemInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Значение;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Свойство;
+    }
+}
+
---
+++ C#/Pr1.1/Pr1.1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr1._1
+{
+    public partial class ОС : Form
+    {
+        public ОС()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSystemInfo();
+        }
+        private void LoadSystemInfo()
+        {
+            dataGridViewSystemInfo.Rows.Clear();
+            dataGridViewSystemInfo.Rows.Add("Версия Windows", Environment.OSVersion);
+            dataGridViewSystemInfo.Rows.Add("64-битная ОС", Environment.Is64BitOperatingSystem);
+            dataGridViewSystemInfo.Rows.Add("Имя компьютера", Environment.MachineName);
+            dataGridViewSystemInfo.Rows.Add("Число процессоров", Environment.ProcessorCount);
+            dataGridViewSystemInfo.Rows.Add("Системная папка", Environment.SystemDirectory);
+            dataGridViewSystemInfo.Rows.Add("Логические диски", string.Join(", ", Environment.GetLogicalDrives()));
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult rez = MessageBox.Show("Вы уверены что хотите выйти?",
+                                 "Подтверждение",
+                                 MessageBoxButtons.YesNo,
+                                 MessageBoxIcon.Question);
+
+            if (rez == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
